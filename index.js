2b509128227be788e3dd80dcb1096ccaa9f3dd32
hugo1987s <hugo.schreiber@hasar.com>
2020-07-02 20:56:19 -0300

    Se agregan rutas de prueba. Obtención de req.body y req.params

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const { json } = require('express');
 const app = express();
 const portNumber = 3344;
 
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.post('/postPage', (req, res) => {
+    console.log(req.body);
+    res.send('Post method executed');
+});
+
+app.put('/putPage', (req, res) => {
+    console.log(req.body);
+    res.send('Put method executed');
+});
+
+app.delete('/deletePage/:codigo', (req, res) => {
+    console.log(req.params);
+    res.send('Delete method executed');
+});
+
 app.listen(portNumber, () => {
-    console.log(`Express server running at port ${portNumber}`);
+    console.log(`Express server running at port ${portNumber}!`);
 });
