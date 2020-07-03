3f4a392b6f22bc7164382e21dfa27207e876b8fc
hugo1987s <hugo.schreiber@hasar.com>
2020-07-02 21:14:59 -0300

    Middleware básico.

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ const portNumber = 3344;
 
 app.use(express.json());
 
+app.all('*', (req, res, next) =>{
+    console.log('Middleware creado');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Home page');
 });
 
-app.post('/postPage', (req, res) => {
+app.post('/user', (req, res) => {
     console.log(req.body);
     res.send('Post method executed');
 });
 
-app.put('/putPage/:codigo', (req, res) => {
+app.put('/user/:codigo', (req, res) => {
     console.log(req.params.codigo);
     console.log(req.body);
     res.send('Put method executed');
 });
 
-app.delete('/deletePage/:codigo', (req, res) => {
+app.delete('/user/:codigo', (req, res) => {
     console.log(req.params.codigo);
     res.send('Delete method executed');
 });
